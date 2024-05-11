f84b2ee16205c53b33edfeaae8088e616ce408f8
Lisi Gonzalez <lisigonzalez@outlook.com>
2024-05-11 00:39:57 -0300

    add(-): create characters static route and dynamic route

---
+++ src/app/characters/[id]/page.js
@@ -0,0 +1,35 @@
+"use client";
+import { getCharacterById } from "@/utils/api";
+import { useState, useEffect } from "react";
+
+export default function CharacterDetail({ params }) {
+  const { id } = params;
+  const [character, setCharacter] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const characterData = await getCharacterById(id);
+      setCharacter(characterData);
+    };
+    fetchData();
+  }, []);
+
+  if (!character) {
+    return <div>Cargando...</div>;
+  }
+
+  return (
+    <div>
+      <h1>Detalle del Personaje: {character.name}</h1>
+      <ul>
+        <li>Nombre: {character.name}</li>
+        {character.eye_color && character.eye_color !== "n/a" && (
+          <li>Color de ojos: {character.eye_color}</li>
+        )}
+        {character.gender && character.gender !== "n/a" && (
+          <li>Género: {character.gender}</li>
+        )}
+      </ul>
+    </div>
+  );
+}
---
+++ src/app/characters/page.js
@@ -0,0 +1,41 @@
+'use client'
+import { getAllCharacters } from "@/utils/api";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+
+export default function Characters(){
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(()=>{
+        const fetchData = async () =>{
+            const charactersData = await getAllCharacters();
+            setCharacters(charactersData);
+        };
+        fetchData()
+    }, []);
+
+    return(
+        <div>
+            <h1>Listado de Personajes</h1>
+            <ul>
+                {characters.map((character)=> {
+                    const arrayId = character.url.split('/')
+                    const idChracter = arrayId[arrayId.length-2]
+                    return <div key={character.idChracter}>
+                             <Link href={`/characters/${idChracter}`}>
+                              {character.name}
+                             </Link>
+                             <li>Nombre: {character.name}</li>
+                                 {character.eye_color && character.eye_color !== 'n/a' && (
+                             <li>Color de ojos: {character.eye_color}</li>
+                                 )}
+                                 {character.gender && character.gender !== 'n/a' && (
+                            <li>Género: {character.gender}</li>
+                                 )}
+                             </div> })
+
+                }
+            </ul>
+        </div>
+    )
+}
