2ae0407e1f58c0b281b20b7bc3d01317af874577
Lisi Gonzalez <lisigonzalez@outlook.com>
2024-05-12 18:16:51 -0300

    add(-): created component loader and added images

--- src/app/characters/[id]/page.js
+++ src/app/characters/[id]/page.js
@@ -1,6 +1,8 @@
 "use client";
 import { getCharacterById } from "@/utils/api";
 import { useState, useEffect } from "react";
+import Loader from "@/components/Loader";
+import Image from "next/image";
 
 export default function CharacterDetail({ params }) {
   const { id } = params;
@@ -15,12 +17,13 @@ export default function CharacterDetail({ params }) {
   }, []);
 
   if (!character) {
-    return <div>Cargando...</div>;
+    return <div><Loader /></div>;
   }
 
   return (
     <div>
       <h1 className="flex justify-center font-bold">Detalle del Personaje: {character.name}</h1>
+      
       <ul className="w-full sm:w-1/2  lg:w-1/4 xl:w-1/5 py-2 text-accent rounded overflow-hidden shadow-lg">
         <li>Nombre: {character.name}</li>
         {character.eye_color && character.eye_color !== "n/a" && (
@@ -29,7 +32,7 @@ export default function CharacterDetail({ params }) {
         {character.gender && character.gender !== "n/a" && (
           <li>Género: {character.gender}</li>
         )}
-      </ul>
+      </ul><Image src="/public/imgCharcter.jpg"  height={100} width={50}/>
     </div>
   );
 }
