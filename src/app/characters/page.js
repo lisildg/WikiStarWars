e62965c2ff63babc36ccb02983b5ccad42e59ced
Lisi Gonzalez <lisigonzalez@outlook.com>
2024-05-12 23:52:14 -0300

    add(-): Added loader on characters and films

--- src/app/characters/page.js
+++ src/app/characters/page.js
@@ -3,6 +3,7 @@ import { getAllCharacters } from "@/utils/api";
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import Loader from "@/components/Loader";
 
 export default function Characters() {
   const [characters, setCharacters] = useState([]);
@@ -18,33 +19,40 @@ export default function Characters() {
   return (
     <div>
       <h1 className="flex justify-center font-bold">Listado de Personajes</h1>
-      <ul className="flex flex-wrap justify-center px-6 py-4">
-        {characters.map((character) => {
-          const arrayId = character.url.split("/");
-          const idChracter = arrayId[arrayId.length - 2];
-          return (
-            <div
-              key={character.idChracter}
-              className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 px-4 py-2 text-accent rounded overflow-hidden shadow-lg"
-            >
-              <Image src="/imgCharcter.jpg" height={200} width={100} />{" "}
-              <Link
-                href={`/characters/${idChracter}`}
-                className="font-bold text-xl mb-2"
+      {characters.length === 0 ? (
+        <div className="flex items-center justify-center h-screen">
+          <Loader />
+        </div>
+      ) : (
+        <ul className="flex flex-wrap justify-center px-6 py-4">
+          {characters.map((character) => {
+            const arrayId = character.url.split("/");
+            const idChracter = arrayId[arrayId.length - 2];
+            return (
+              <div
+                key={character.idChracter}
+                className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 px-4 py-2 text-accent rounded overflow-hidden shadow-lg"
               >
-                {character.name}
-              </Link>
-              <li>Nombre: {character.name}</li>
-              {character.eye_color && character.eye_color !== "n/a" && (
-                <li>Color de ojos: {character.eye_color}</li>
-              )}
-              {character.gender && character.gender !== "n/a" && (
-                <li>Género: {character.gender}</li>
-              )}
-            </div>
-          );
-        })}
-      </ul>
+                <Image src="/imgCharcter.jpg" height={200} width={100} />{" "}
+                <Link
+                  href={`/characters/${idChracter}`}
+                  className="font-bold text-xl mb-2"
+                >
+                  {character.name}
+                </Link>
+                <li>Nombre: {character.name}</li>
+                {character.eye_color && character.eye_color !== "n/a" && (
+                  <li>Color de ojos: {character.eye_color}</li>
+                )}
+                {character.gender && character.gender !== "n/a" && (
+                  <li>Género: {character.gender}</li>
+                )}
+              </div>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
+  
 }
--- src/app/films/page.js
+++ src/app/films/page.js
@@ -3,6 +3,7 @@ import { getAllFilms } from "@/utils/api";
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import Loader from "@/components/Loader";
 
 export default function Films() {
   const [films, setFilms] = useState([]);
@@ -18,22 +19,29 @@ export default function Films() {
   return (
     <div>
       <h1 className="flex justify-center font-bold">Listado de Films</h1>
-      <ul className="flex flex-wrap justify-center px-6 py-4">
-        {films.map((film) => {
-          const arrayId = film.url.split("/");
-          const idFilm = arrayId[arrayId.length - 2];
-          return (
-            <div
-              key={film.idFilm}
-              className="w-full sm:w-1/2  lg:w-1/4 xl:w-1/5 flex justify-center  py-2 text-accent rounded overflow-hidden shadow-lg"
-            >
-              <Image src="/imgFilm.jpg" height={200} width={100} />{" "}
-              <Link href={`/films/${idFilm}`}>{film.title}</Link>
-              <li>Episodio: {film.episode_id}</li>
-            </div>
-          );
-        })}
-      </ul>
+      {films.length === 0 ? (
+        <div className="flex items-center justify-center h-screen">
+          <Loader />
+        </div>
+      ) : (
+        <ul className="flex flex-wrap justify-center px-6 py-4">
+          {films.map((film) => {
+            const arrayId = film.url.split("/");
+            const idFilm = arrayId[arrayId.length - 2];
+            return (
+              <div
+                key={film.idFilm}
+                className="w-full sm:w-1/2  lg:w-1/4 xl:w-1/5 flex justify-center  py-2 text-accent rounded overflow-hidden shadow-lg"
+              >
+                <Image src="/imgFilm.jpg" height={200} width={100} />{" "}
+                <Link href={`/films/${idFilm}`}>{film.title}</Link>
+                <li>Episodio: {film.episode_id}</li>
+              </div>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
+  
 }
